2768a854e74fa58564b803922d1103de5adfd237
hiroshi.matsui <hiroshi.matsui@STTA18006-Hiroshi-Matsui.local>
2019-08-24 16:44:38 +0900

    Fix a typo

--- chainerdml/functions/__init__.py
+++ chainerdml/functions/__init__.py
@@ -1,4 +1,4 @@
-from chaienrdml.functions.loss.npair_hinge import npair_hinge
+from chainerdml.functions.loss.npair_hinge import npair_hinge
 
 from chainerdml.functions.math.euclidean_pairwise_distances import (
     euclidean_pairwise_distances,
